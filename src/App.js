ae02921bbd9c4158cec69b3b1c62b64aec9af93b
Onimison <godblessbashir@gmail.com>
2022-10-31 06:11:52 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Profile from './components/Profile/profile'
+import Links from './components/Links/links'
+import Footer from './components/Footer/footer'
+const App = () => {
+
+    return (
+        <>
+            <Profile />
+            <Links />
+            <Footer />
+        </>
+
+    )
+}
+
+export default App
